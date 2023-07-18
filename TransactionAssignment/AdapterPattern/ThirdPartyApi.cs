4d07474096100baae5eb4623bf53ea8750dfa220
PUSHAN GHOSH <pushanghosh9@gmail.com>
2023-07-18 20:43:34 +0530

    Added adapter pattern sample

---
+++ TransactionAssignment/AdapterPattern/ThirdPartyApi.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System.Xml.Linq;
+
+namespace TransactionAssignment.AdapterPattern
+{
+    public class ThirdPartyApi
+    {
+        public string GetTransactionsJson()
+        {
+            return JsonConvert.SerializeObject(TransactionDataProvider.GetTransactions);
+        }
+
+        public XDocument GetTransactionsXml()
+        {
+
+            var xDocument = new XDocument();
+
+            var xElement = new XElement("Transactions", TransactionDataProvider.GetTransactions
+                .Select(m => new XElement("Transaction",
+                                        new XElement("TransactionId", m.TransactionId),
+                                        new XElement("Amount", m.Amount),
+                                        new XElement("CurrencyCode", m.CurrencyCode),
+                                        new XElement("TransactionDate", m.TransactionDate),
+                                        new XElement("Status", m.Status)
+                                    )));
+            xDocument.Add(xElement);
+            return xDocument;
+        }
+    }
+}
---
+++ TransactionAssignment/AdapterPattern/TransactionDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using TransactionAssignment.Models;
+
+namespace TransactionAssignment.AdapterPattern
+{
+    public static class TransactionDataProvider
+    {
+        public static List<TransactionModel> GetTransactions =>
+            new List<TransactionModel>()
+            {
+                new TransactionModel() { Amount =  100, CurrencyCode="USD",TransactionDate=new DateTime(2023,07,01),TransactionId = Guid.NewGuid().ToString(), Status="A" },
+                new TransactionModel() { Amount =  200, CurrencyCode="SGD",TransactionDate=new DateTime(2023,07,02),TransactionId = Guid.NewGuid().ToString(), Status="RF" },
+                new TransactionModel() { Amount =  300, CurrencyCode="VND",TransactionDate=new DateTime(2023,07,03),TransactionId = Guid.NewGuid().ToString(), Status="CB" }
+            };
+    }
+}
