6a893887f0aedc366acd075152b1ccb29f97cba8
ashish <ashish.bhi@2c2pexternal.com>
2023-08-17 21:27:48 +0530

    bridge design pattern

---
+++ BridgeDesignPattern/Models.cs
@@ -0,0 +1,77 @@
+﻿namespace BridgeDesignPattern
+{
+    public interface IFileOperation
+    {
+        void SaveData();
+        void DeleteData();
+    }
+
+    public class FileOperation : IFileOperation
+    {
+        public void SaveData() 
+        {
+            Console.WriteLine("Saved Data to file");
+        }
+
+        public void DeleteData()
+        {
+            Console.WriteLine("Deleted Data from file");
+        }
+    }
+
+    public class DatabaseOperation : IFileOperation 
+    {
+        public void SaveData()
+        {
+            Console.WriteLine("Saved Data to database");
+        }
+
+        public void DeleteData()
+        {
+            Console.WriteLine("Deleted Data from database");
+        }
+    }
+
+    public abstract class FileOperationAbstract
+    {
+        protected IFileOperation fileOperation;
+        public abstract void SaveData();
+        public abstract void DeleteData();
+    }
+
+    public class FileConrete : FileOperationAbstract
+    {
+        public FileConrete(IFileOperation fileOperation)
+        {
+            this.fileOperation = fileOperation;
+        }
+
+        public override void DeleteData()
+        {
+            fileOperation.DeleteData();
+        }
+
+        public override void SaveData()
+        {
+            fileOperation.SaveData();
+        }
+    }
+
+    public class DatabaseConcrete : FileOperationAbstract
+    {
+        public DatabaseConcrete(IFileOperation fileOperation)
+        {
+            this.fileOperation = fileOperation;
+        }
+
+        public override void DeleteData()
+        {
+            fileOperation.DeleteData();
+        }
+
+        public override void SaveData()
+        {
+            fileOperation.SaveData();
+        }
+    }
+}
---
+++ BridgeDesignPattern/Program.cs
@@ -0,0 +1,13 @@
+﻿using BridgeDesignPattern;
+
+IFileOperation fileOperation = new FileOperation();
+IFileOperation databaseOperation = new DatabaseOperation();
+
+FileOperationAbstract file = new FileConrete(fileOperation);
+FileOperationAbstract db = new DatabaseConcrete(databaseOperation);
+
+file.SaveData();
+file.DeleteData();
+
+db.DeleteData();
+db.SaveData();
